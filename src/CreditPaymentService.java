589d7aba3626f5bb528c221beb8f0756296dbb29
Alexander Syayalev <alexxs@inbox.ru>
2022-01-13 13:08:46 +0300

    Reformat Code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,13 @@
 //Источник https://journal.tinkoff.ru/guide/credit-payment/
 
 public class CreditPaymentService {
-    public double calculate (int creditTime, double creditTax, int amount) {
-        int creditTimeInMon = creditTime*12;
+    public double calculate(int creditTime, double creditTax, int amount) {
+        int creditTimeInMon = creditTime * 12;
         double paymentCount = creditTimeInMon; //введен для понятности кода (названия переменных)
-        double taxInMon = creditTax/12/100;
-        double annuitPreCount = Math.pow((taxInMon+1),paymentCount); //введен для повышения читаемости
-        double annuitKoef = taxInMon*(annuitPreCount/(annuitPreCount-1));
-        double result = amount*annuitKoef; //можно объединить с предыдущей строкой, оставлено для наглядности
+        double taxInMon = creditTax / 12 / 100;
+        double annuitPreCount = Math.pow((taxInMon + 1), paymentCount); //введен для повышения читаемости
+        double annuitKoef = taxInMon * (annuitPreCount / (annuitPreCount - 1));
+        double result = amount * annuitKoef; //можно объединить с предыдущей строкой, оставлено для наглядности
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
         CreditPaymentService service = new CreditPaymentService();
 
-        System.out.printf("%.2f", service.calculate(1,9.99, 1000000));
+        System.out.printf("%.2f", service.calculate(1, 9.99, 1000000));
         System.out.println("  ");
-        System.out.printf("%.2f", service.calculate(2,9.99, 1000000));
+        System.out.printf("%.2f", service.calculate(2, 9.99, 1000000));
         System.out.println("  ");
-        System.out.printf("%.2f", service.calculate(3,9.99, 1000000));
+        System.out.printf("%.2f", service.calculate(3, 9.99, 1000000));
 
     }
 }
